23b512bea4e623e7f011ced3aabd3f9c6e9d9120
Александр Черных <55315983+TermosW@users.noreply.github.com>
2022-01-09 05:45:04 +0500

    Add files via upload

---
+++ test1/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using test1.ViewModels;
+using test1.Views;
+using Xamarin.Forms;
+
+namespace test1
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
