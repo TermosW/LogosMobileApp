fe12d9d8c9c532ebd6573dcbe375ba1abebb72dc
Александр Черных <55315983+TermosW@users.noreply.github.com>
2022-01-09 05:46:08 +0500

    Add files via upload

---
+++ test1/Views/ExercisesList.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using test1.ViewModels;
+
+namespace test1.Views
+{
+    public partial class ExercisesList : ContentPage
+    {
+        public ExercisesList()
+        {
+            InitializeComponent();
+            this.BindingContext = new ExercisesListViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            ExDictionary.exElements();
+
+            ex1Title.Text = ExDictionary.Exercises[0].Title;
+            ex1Text.Text = ExDictionary.Exercises[0].Text.Substring(0, 50) + " ...";
+            ex1Time.Text = ExDictionary.Exercises[0].Time.ToString() + " сек";
+
+            ex2Title.Text = ExDictionary.Exercises[1].Title;
+            ex2Text.Text = ExDictionary.Exercises[1].Text.Substring(0, 47) + " ...";
+            ex2Time.Text = ExDictionary.Exercises[1].Time.ToString() + " сек";
+
+            ex3Title.Text = ExDictionary.Exercises[2].Title;
+            ex3Text.Text = ExDictionary.Exercises[2].Text.Substring(0, 50) + " ...";
+            ex3Time.Text = ExDictionary.Exercises[2].Time.ToString() + " сек";
+
+            ex4Title.Text = ExDictionary.Exercises[3].Title;
+            ex4Text.Text = ExDictionary.Exercises[3].Text.Substring(0, 50) + " ...";
+            ex4Time.Text = ExDictionary.Exercises[3].Time.ToString() + " сек";
+
+            ex5Title.Text = ExDictionary.Exercises[4].Title;
+            ex5Text.Text = ExDictionary.Exercises[4].Text.Substring(0, 50) + " ...";
+            ex5Time.Text = ExDictionary.Exercises[4].Time.ToString() + " сек";
+
+            base.OnAppearing();
+        }
+    }
+}
---
+++ test1/Views/FullExercise.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using test1.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace test1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FullExercise : ContentPage
+    {
+        public FullExercise()
+        {
+            InitializeComponent();
+            this.BindingContext = new FullExerciseViewModel();
+        }
+
+        //protected override void OnAppearing()
+        //{
+        //    i = 0;
+        //    arrowLeft.IsEnabled = false;
+        //    arrowLeft.Opacity = 0;
+
+        //    exText.Text = ExDictionary.Exercises[0].Text;
+        //    i = 0;
+
+        //    base.OnAppearing();
+        //}
+
+        //void OnImageArrowLeftTapped(object sender, EventArgs args)
+        //{
+        //    try
+        //    {
+        //        i--;
+        //        exText.Text = ExDictionary.Exercises[i].Text;
+        //        arrowRight.IsEnabled = true;
+        //        arrowRight.Opacity = 1;
+        //        if (i == 0)
+        //        {
+        //            arrowLeft.IsEnabled = false;
+        //            arrowLeft.Opacity = 0;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+
+        //void OnImageArrowRightTapped(object sender, EventArgs args)
+        //{
+            
+        //    try
+        //    {
+        //        i++;
+        //        arrowLeft.IsEnabled = true;
+        //        arrowLeft.Opacity = 1;
+        //        exText.Text = ExDictionary.Exercises[i].Text;
+        //        if (i == 5)
+        //        {
+        //            arrowRight.IsEnabled = false;
+        //            arrowRight.Opacity = 0;
+        //        }
+                
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+    }
+}
